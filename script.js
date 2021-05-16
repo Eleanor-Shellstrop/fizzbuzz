9ceac2d1d93f0c578931d3fa7e693398f0a2c30a
Anne Ensign <anne.ensign@gmail.com>
2021-05-16 11:14:22 -0400

    Add class toggle to JS for styles

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ for (let i = 1; i <= 100; i++) {
     //  Step 4: Change every multiple of both 3 and 5 to "FizzBuzz"
     if (eachNum.innerText % 15 === 0) {
         eachNum.innerText = "FizzBuzz";
+        eachNum.classList.toggle("fizzbuzz");
     }
     
     //  Step 2: Change every 3rd number to "Fizz"
     if (eachNum.innerText % 3 === 0) {
         eachNum.innerText = "Fizz";
+        eachNum.classList.toggle("fizz");
     }
 
     //  Step 3: Change every 5th number to "Buzz"
     if (eachNum.innerText % 5 === 0) {
         eachNum.innerText = "Buzz";
+        eachNum.classList.toggle("buzz");
     }
 }
